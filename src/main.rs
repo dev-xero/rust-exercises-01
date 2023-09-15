98a3b54b397f1b3b1519c45b9b32abcca1e82945
Xero kun <elo48858@gmail.com>
2023-09-16 00:45:19 +0100

    `FEAT` Implemented nth fib algorithm

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,28 @@ fn convert_temperature(temp: i32, to: Temp) -> i32 {
     };
 }
 
+fn nth_fib(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    }
+
+    if n == 1 {
+        return 1;
+    }
+
+    let mut curr = 1;
+    let mut _prev = 1;
+
+    for _ in 4..=n {
+        let temp = curr;
+
+        curr += _prev;
+        _prev = temp;
+    }
+
+    return curr;
+}
+
 fn main() {
     println!("Rust exercises");
 
@@ -37,4 +59,13 @@ fn main() {
     println!("96F to celsius is: {body_temp}C");
     println!("Absolute zero, {absolute_zero}K is: -273 C");
     println!("The boiling point of pure water is: {water_boiling_point} F");
+
+    println!("\nFibonacci Sequences");
+    println!("---");
+
+    let fifth_fib = nth_fib(5);
+    let tenth_fib = nth_fib(10);
+
+    println!("The fifth fib term is: {fifth_fib}");
+    println!("The tenth fib term is: {tenth_fib}");
 }
