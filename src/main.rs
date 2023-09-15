f74ca4b4b1401d7ad1fe31add34e51c0f929e18a
Xero kun <elo48858@gmail.com>
2023-09-16 00:14:50 +0100

    `FEAT` Implemented temperature conversion algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+enum Temp {
+    Celsius,
+    Fahrenheit
+}
+
+fn convert_temperature(temp: i32, to: Temp) -> i32 {
+    fn to_celsius(temp: i32) -> i32 {
+        return (temp - 32) * 5/9;
+    }
+
+    fn to_fahrenheit(temp: i32) -> i32 {
+        return (temp * 9/5) + 32;
+    }
+
+    return match to {
+        Temp::Celsius => { to_celsius(temp) },
+        Temp::Fahrenheit => { to_fahrenheit(temp) }
+    };
+}
+
 fn main() {
     println!("Rust exercises");
+
+    println!("\nTemperature Conversion");
+    println!("---");
+
+    let body_temp = convert_temperature(97, Temp::Celsius);
+    println!("96F to celsius is: {body_temp}C");
 }
